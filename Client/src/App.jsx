7a335f7206ef0e045f20bb2179ebfeba389753c6
Krish Yadav <75037831+krrishexe@users.noreply.github.com>
2024-04-01 23:49:41 +0530

    keep init commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import MiniDrawer from './Components/MiniDrawer'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <MiniDrawer />
+    </>
+  )
+}
+
+export default App
