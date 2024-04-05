6bf584bb57a1b32d93a6828a1fbf6b0fa67de33c
Krish Yadav <75037831+krrishexe@users.noreply.github.com>
2024-04-05 13:34:11 +0530

    added editable content dialog box

--- Client/src/Components/notes/Notemap.jsx
+++ Client/src/Components/notes/Notemap.jsx
@@ -6,6 +6,14 @@ import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { deletedNotes } from '../../store/atoms/DeletedNotes';
 import { archiveNotes } from "../../store/atoms/ArchiveNotes"
 import { todoListState } from "../../store/atoms/Todo"
+import EditIcon from '@mui/icons-material/Edit';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function Notemap({ todoList, expandedNote, handleNoteClick }) {
 
@@ -13,6 +21,8 @@ function Notemap({ todoList, expandedNote, handleNoteClick }) {
     const [archivedTodo, setArchivedTodo] = useRecoilState(archiveNotes)
     const [deletedTodo, setDeletedTodo] = useRecoilState(deletedNotes)
     const [currentPage, setCurrentPage] = useState(1);
+    const [editDialogOpen, setEditDialogOpen] = useState(false);
+    const [editedNote, setEditedNote] = useState(null);
 
 
     const notesPerPage = 6;
@@ -25,6 +35,25 @@ function Notemap({ todoList, expandedNote, handleNoteClick }) {
         setTodoList(updatedNote)
         setArchivedTodo([note, ...archivedTodo])
     }
+    const handleEdit = (note) => {
+        setEditedNote(note);
+        setEditDialogOpen(true);
+    }
+
+    const handleNoteChange = (event) => {
+        setEditedNote({
+            ...editedNote,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleEditSubmit = () => {
+        const updatedNotes = todoList.map((note) =>
+            note.id === editedNote.id ? editedNote : note
+        );
+        setTodoList(updatedNotes);
+        setEditDialogOpen(false);
+    };
 
     const handleAddDelete = (note) => {
         const updatedNote = todoList.filter((note) => note.id !== expandedNote)
@@ -57,6 +86,9 @@ function Notemap({ todoList, expandedNote, handleNoteClick }) {
                                     <p>{note.description}</p>
                                 </div>
                                 <div className='icon mr-4'>
+                                    <IconButton onClick={() => handleEdit(note)}>
+                                        <EditIcon fontSize='small' style={{ color: '#fff' }} />
+                                    </IconButton>
                                     <IconButton onClick={() => handleAddArchive(note)}>
                                         <ArchiveIcon fontSize='small' style={{ color: '#fff' }} />
                                     </IconButton>
@@ -68,6 +100,38 @@ function Notemap({ todoList, expandedNote, handleNoteClick }) {
                         </label>
                     </>
                 ))}
+                <Dialog open={editDialogOpen} onClose={() => setEditDialogOpen(false)}>
+                    <DialogTitle style={{ background: '#565758' }} className='text-white'>Edit Note</DialogTitle>
+                    <DialogContent >
+                        <DialogContentText style={{ marginTop: '10px' }}>
+                            Please enter the new title and description for the note.
+                        </DialogContentText>
+                        <TextField
+                            style={{ marginTop: '10px' }}
+                            autoFocus
+                            margin="normal"
+                            name="name"
+                            label="Title"
+                            type="text"
+                            fullWidth
+                            value={editedNote?.name}
+                            onChange={handleNoteChange}
+                        />
+                        <TextField
+                            margin="dense"
+                            name="description"
+                            label="Description"
+                            type="text"
+                            fullWidth
+                            value={editedNote?.description}
+                            onChange={handleNoteChange}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => setEditDialogOpen(false)}>Cancel</Button>
+                        <Button onClick={handleEditSubmit}>Save</Button>
+                    </DialogActions>
+                </Dialog>
             </div>
             {todoList.length > notesPerPage && <div className='mt-10'>
                 <button className='p-3 mr-10  rounded-lg disabled:bg-gray-700 text-gray-400' onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
